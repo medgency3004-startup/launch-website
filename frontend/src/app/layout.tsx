f8a3b6b4e356c9f62650518b3792b242e8ccf452
HimanshuCodes23 <himanshucodes23@gmail.com>
2026-02-04 19:57:55 +0530

    vercel analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "Medgency",
